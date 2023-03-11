5887cb4eaecf232cd6c559065a6d1cfcfa90f9f5
auadmendes <luciano.auad@gmail.com>
2023-03-11 17:47:40 -0300

    clear

--- src/pages/api/draftkings_vip.ts
+++ src/pages/api/draftkings_vip.ts
@@ -61,73 +61,6 @@ export default async function handler(req: DraftkingsRequest, res: NextApiRespon
     await page.keyboard.press('Enter', { delay: 100 })
 
 
-    // beginning of FOR
-
-    // for (let i = 0; i < ptxArray.length; i++) {
-    //   let ref = ptxArray[i]
-    //   let trIds = ''
-    //   let trxType = ''
-    //   let trxMerchantName = ''
-
-    // await page.waitForSelector('input[name="ppTransactionId"]')
-    // await page.type('input[name="ppTransactionId"]', ref)
-
-    // await page.keyboard.press('Enter', { delay: 100 })
-
-    // await page.waitForSelector('.break-all', { delay: 100 })
-
-    // const referenceId = await page.$eval(
-    //   '.break-all',
-    //   el => el.textContent
-    // )
-
-    // trxType = await page.$$eval('table tr td', anchors => {
-    //   return anchors.map(links => links.textContent).slice(5, 6)
-    // })
-
-    // trxMerchantName = await page.$$eval('table tr td', anchors => {
-    //   return anchors.map(links => links.textContent).slice(13, 14)
-    // })
-
-    // if (trxType[0] === 'External') {
-    //   trIds = await page.$$eval('table tr td a', anchors => {
-    //     return anchors.map(links => links.textContent).slice(7, 8)
-    //   })
-    // } else {
-    //   trIds = await page.$$eval('table tr td a', anchors => {
-    //     return anchors.map(links => links.textContent).slice(0, 1)
-    //   })
-    // }
-
-    // const amount = await page.$$eval('table tr td', anchors => {
-    //   return anchors.map(links => links.textContent).slice(15, 16)
-    // })
-
-    // infoArray.push({
-    //   merchantName: trxMerchantName[0],
-    //   transactionId: trIds[0],
-    //   merchantReference: referenceId,
-    //   amount: 'USD ' + req.body.refs[i][3],
-    //   reasonCode: 'Insufficient Funds',
-    //   reason: 'R01',
-    // } as TransactionInfo)
-
-    // await page.waitForSelector('input[name="ppTransactionId"]')
-
-    // const ppTransactionId = await page.waitForSelector(
-    //   'input[name="ppTransactionId"]'
-    // )
-
-    // await ppTransactionId.click({ clickCount: 3 })
-    // await ppTransactionId.press('Backspace')
-
-    // }
-
-    //end of FOR
-
-
-    //
-
     for (const ref of ptxArray) {
       await page.waitForSelector('input[name="ppTransactionId"]')
       await page.type('input[name="ppTransactionId"]', ref)
@@ -135,13 +68,13 @@ export default async function handler(req: DraftkingsRequest, res: NextApiRespon
       await page.waitForSelector('.break-all', { delay: 100 })
     
       const referenceId = await page.$eval('.break-all', el => el.textContent)
-      // const trxType = await page.$$eval('table tr td', anchors => anchors.map(links => links.textContent).slice(5, 6))
+
       const trxMerchantName = await page.$$eval('table tr td', anchors => anchors.map(links => links.textContent).slice(13, 14))
       const trIds = await page.$$eval('table tr td a', anchors => {
         const sliceStart = (anchors[5].textContent === 'External') ? 7 : 0
         return anchors.map(links => links.textContent).slice(sliceStart, sliceStart + 1)
       })
-      // const amount = await page.$$eval('table tr td', anchors => anchors.map(links => links.textContent).slice(15, 16))
+
     
       infoArray.push({
         merchantName: trxMerchantName[0],
