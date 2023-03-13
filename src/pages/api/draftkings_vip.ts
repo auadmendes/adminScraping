d3c5ff8aa81c1b56f7eb637a1f56ac47e01cab30
auadmendes <luciano.auad@gmail.com>
2023-03-13 11:08:20 -0300

    draftkings

--- src/pages/api/draftkings_vip.ts
+++ src/pages/api/draftkings_vip.ts
@@ -14,84 +14,86 @@ interface DraftkingsRequest extends NextApiRequest {
 
 export default async function handler(req: DraftkingsRequest, res: NextApiResponse) {
 
-  const USER_LOGIN = req.body.user
-  const USER_PASSWORD = req.body.password
+  // const USER_LOGIN = req.body.user
+  // const USER_PASSWORD = req.body.password
 
-  let browser = null;
+  // let browser = null;
 
-  const aArray = []
+  // const aArray = []
 
-  req.body.refs.map(item => {
-    aArray.push(item[6])
+  // req.body.refs.map(item => {
+  //   aArray.push(item[6])
    
-  })
+  // })
 
-  const ptxArray = aArray
+  // const ptxArray = aArray
 
-  const infoArray = []
+  // const infoArray = []
 
-  try {
+  // try {
 
-    const options = await getOptions(true)
-    browser = await puppeteer.launch(options)
+  //   const options = await getOptions(true)
+  //   browser = await puppeteer.launch(options)
 
-    let page = await browser.newPage();
+  //   let page = await browser.newPage();
 
-    await page.goto(req.body.url);
+  //   await page.goto(req.body.url);
 
-    await page.waitForSelector('input[name="username"]')
-    await page.type('input[name="username"]', `${USER_LOGIN}`)
+  //   await page.waitForSelector('input[name="username"]')
+  //   await page.type('input[name="username"]', `${USER_LOGIN}`)
   
-    await page.waitForSelector('input[name="password"]')
-    await page.type('input[name="password"]', `${USER_PASSWORD}`, { delay: 50 })
+  //   await page.waitForSelector('input[name="password"]')
+  //   await page.type('input[name="password"]', `${USER_PASSWORD}`, { delay: 50 })
   
-    await page.waitForSelector('input[name="mfa_code"]')
-    await page.type('input[name="mfa_code"]', `${req.body.mfa}`, { delay: 50 })
+  //   await page.waitForSelector('input[name="mfa_code"]')
+  //   await page.type('input[name="mfa_code"]', `${req.body.mfa}`, { delay: 50 })
   
-    await page.keyboard.press('Enter', { delay: 100 })
+  //   await page.keyboard.press('Enter', { delay: 100 })
 
 
-    for (const ref of ptxArray) {
+  //   for (const ref of ptxArray) {
       
-      await page.waitForSelector('input[name="ppTransactionId"]')
-      await page.type('input[name="ppTransactionId"]', ref)
-      await page.keyboard.press('Enter', { delay: 100 })
-      await page.waitForSelector('.break-all', { delay: 100 })
+  //     await page.waitForSelector('input[name="ppTransactionId"]')
+  //     await page.type('input[name="ppTransactionId"]', ref)
+  //     await page.keyboard.press('Enter', { delay: 100 })
+  //     await page.waitForSelector('.break-all', { delay: 100 })
     
-      const referenceId = await page.$eval('.break-all', el => el.textContent)
+  //     const referenceId = await page.$eval('.break-all', el => el.textContent)
 
-      const trxMerchantName = await page.$$eval('table tr td', anchors => anchors.map(links => links.textContent).slice(13, 14))
-      const trIds = await page.$$eval('table tr td a', anchors => {
-        const sliceStart = (anchors[5].textContent === 'External') ? 7 : 0
-        return anchors.map(links => links.textContent).slice(sliceStart, sliceStart + 1)
-      })
+  //     const trxMerchantName = await page.$$eval('table tr td', anchors => anchors.map(links => links.textContent).slice(13, 14))
+  //     const trIds = await page.$$eval('table tr td a', anchors => {
+  //       const sliceStart = (anchors[5].textContent === 'External') ? 7 : 0
+  //       return anchors.map(links => links.textContent).slice(sliceStart, sliceStart + 1)
+  //     })
 
     
-      infoArray.push({
-        merchantName: trxMerchantName[0],
-        transactionId: trIds[0],
-        merchantReference: referenceId,
-        amount: 'USD ' + req.body.refs[ptxArray.indexOf(ref)][3],
-        reasonCode: 'Insufficient Funds',
-        reason: 'R01',
-      })
+  //     infoArray.push({
+  //       merchantName: trxMerchantName[0],
+  //       transactionId: trIds[0],
+  //       merchantReference: referenceId,
+  //       amount: 'USD ' + req.body.refs[ptxArray.indexOf(ref)][3],
+  //       reasonCode: 'Insufficient Funds',
+  //       reason: 'R01',
+  //     })
     
-      const ppTransactionId = await page.waitForSelector('input[name="ppTransactionId"]')
-      await ppTransactionId.click({ clickCount: 3 })
-      await ppTransactionId.press('Backspace')
-    }
+  //     const ppTransactionId = await page.waitForSelector('input[name="ppTransactionId"]')
+  //     await ppTransactionId.click({ clickCount: 3 })
+  //     await ppTransactionId.press('Backspace')
+  //   }
 
-    //
+  //   //
 
-  } catch (error) {
-    console.log('Olha o erro' + error)
+  // } catch (error) {
+  //   console.log('Olha o erro' + error)
 
-  } finally {
-    if (browser !== null) {
-    }
-  }
+  // } finally {
+  //   if (browser !== null) {
+  //   }
+  // }
 
-  await browser.close()
+  // await browser.close()
 
-  res.status(201).send(infoArray)
+  // res.status(201).send(infoArray)
+
+  res.status(200).send('olha isso')
 }
